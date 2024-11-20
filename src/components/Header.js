92b44ac1df3d6fbc4be3207f927f7a3b0a2030ff
MahaGit1233 <maharush5409@gmail.com>
2024-11-20 15:15:14 +0530

    content commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import './Header.css';
 
 const Header = () => {
-    return <div>
+    return <div className="maindiv">
         <header className="header">
             <h1>React Meals</h1>
             <button>
@@ -10,6 +10,11 @@ const Header = () => {
             </button>
         </header>
         <img src="https://i.pinimg.com/736x/06/ed/42/06ed4211eff090878101f035a02a9e31.jpg" />
+        <div className="content">
+            <h2>Delicious Food, Delivered To You</h2>
+            <p>Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>
+            <p>All our meals are cooked with high-quality ingredients, just-in-time and ofcourse by experienced chefs!</p>
+        </div>
     </div>
 }
 
